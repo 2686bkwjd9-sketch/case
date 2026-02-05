d6f8b1d2265280889ab615c58781b2e716851395
2686bkwjd9-sketch <2686bkwjd9@privaterelay.appleid.com>
2026-02-05 20:05:51 +0700

    добавила квадрат

--- case.py
+++ case.py
@@ -1,1 +1,25 @@
-hello
+# Case-study #1
+# Developers: Voronina A., Sankina A., Grishina D.
+
+import turtle 
+
+def square(x,y,a,color): 
+  turtle.up()
+  turtle.setposition(x, y)
+  turtle.down()
+  turtle.color(color)
+  turtle.begin_fill()
+
+  turtle.forward(a)
+  turtle.right(90)
+  turtle.forward(a)
+  turtle.right(90)
+  turtle.forward(a)
+  turtle.right(90)
+  turtle.forward(a)
+  turtle.right(90)
+
+  turtle.end_fill()
+
+square(-50, 50, 100, "red")
+turtle.done()
